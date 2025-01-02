53df7ba8a516494e19bba883716bc7e0b46b7885
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-01-02 22:27:46 +0100

    Aliased imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
+import path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       "/api-stillness": {
