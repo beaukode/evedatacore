8364b855aa3ef5f1b7c1d0aa3b7141f23e3e19ca
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-01-15 23:04:46 +0100

    Script to build a types JSON with images

---
+++ scripts/data-types.ts
@@ -0,0 +1,52 @@
+// This script create a types data json with image
+// by merging data from https://blockchain-gateway-stillness.live.tech.evefrontier.com/types and
+// https://blockchain-gateway-stillness.live.tech.evefrontier.com/types/{typeId}
+
+import { ensureDirSync } from "fs-extra";
+import { writeFileSync } from "fs";
+import { join } from "path";
+
+const typesUrl = "https://eve.beaukode.net/api-stillness/types";
+
+async function main() {
+  ensureDirSync("./output");
+
+  console.log("Downloading types data...");
+  const response = await fetch(typesUrl);
+  if (!response.ok) {
+    throw new Error("Failed to download solarsystems data");
+  }
+  const typesData = await response.json();
+
+  const total = Object.keys(typesData).length;
+  console.log(`Downloaded ${total} types`);
+
+  let count = 0;
+  let missing = 0;
+  for (const id in typesData) {
+    // console.log(`Loading image ${id}...`);
+    const response = await fetch(`${typesUrl}/${id}`);
+    if (response.ok) {
+      const data = await response.json();
+      if (data.metadata.image) {
+        typesData[id].image = data.metadata.image;
+        count++;
+      } else {
+        console.log(`Missing image for type ${id}`);
+        console.log(data);
+        missing++;
+      }
+    } else {
+      console.log(`Missing data for type ${id}`);
+    }
+  }
+
+  console.log(
+    `Merged ${count} types, ${missing} missing images, ${total} total types`
+  );
+  const outputPath = join("./output", "types.json");
+  writeFileSync(outputPath, JSON.stringify(typesData), "utf-8");
+  console.log(`Types data written to ${outputPath}`);
+}
+
+main();
