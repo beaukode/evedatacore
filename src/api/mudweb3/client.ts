9ed6b76d597dbf62a701ac546982e7c52a574928
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-01-21 22:31:27 +0100

    erc721balanceOf & erc721transferFrom

--- src/api/mudweb3/client.ts
+++ src/api/mudweb3/client.ts
@@ -1,33 +1,55 @@
 import { Table, GetRecordOptions, getRecord } from "@latticexyz/store/internal";
-import { Client, isAddress } from "viem";
+import { Client, Hex, isAddress, WalletClient as ViemWalletClient } from "viem";
 import { eveworld } from "./eveworld";
+import { erc721actions, erc721walletActions } from "./erc721actions";
 
 export type MudWeb3ClientConfig = {
-  ethClient: Client;
+  publicClient: Client;
+  walletClient?: ViemWalletClient;
   worldAddress: string;
 };
 
 export type MudWeb3Client = ReturnType<typeof createMudWeb3Client>;
+type WalletClient = ReturnType<typeof createWalletClient>;
+type PublicClient = ReturnType<typeof createPublicClient>;
 
-export function createMudWeb3Client(config: MudWeb3ClientConfig) {
-  const { worldAddress, ethClient } = config;
-  if (!isAddress(worldAddress)) {
-    throw new Error(`Invalid world address: ${worldAddress}`);
-  }
+function createWalletClient(
+  _worldAddress: Hex,
+  walletClient: ViemWalletClient
+) {
+  return walletClient.extend(erc721walletActions);
+}
 
-  return ethClient
-    .extend((ethClient) => ({
+function createPublicClient(worldAddress: Hex, publicClient: Client) {
+  return publicClient
+    .extend(erc721actions)
+    .extend((client) => ({
       async getRecord<table extends Table>(options: GetRecordOptions<table>) {
-        return getRecord<table>(ethClient, options);
+        return getRecord<table>(client, options);
       },
     }))
-    .extend((ethClient) => ({
+    .extend((client) => ({
       async getDeployableState(id: bigint) {
-        return ethClient.getRecord({
+        return client.getRecord({
           address: worldAddress,
           table: eveworld.tables.eveworld__DeployableState,
           key: { smartObjectId: id },
         });
       },
     }));
+}
+
+export function createMudWeb3Client(config: MudWeb3ClientConfig) {
+  const { worldAddress, publicClient, walletClient } = config;
+  if (!isAddress(worldAddress)) {
+    throw new Error(`Invalid world address: ${worldAddress}`);
+  }
+
+  const extendedClient: PublicClient & { wallet?: WalletClient } =
+    createPublicClient(worldAddress, publicClient);
+
+  if (walletClient) {
+    extendedClient.wallet = createWalletClient(worldAddress, walletClient);
+  }
+  return extendedClient;
 }
--- src/contexts/AppContextProvider.tsx
+++ src/contexts/AppContextProvider.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useClient } from "wagmi";
+import { usePublicClient, useWalletClient } from "wagmi";
 import { AppContext } from "./AppContext";
 import { createMudSqlClient } from "@/api/mudsql";
 import { chainId, indexerBaseUrl, worldAddress } from "@/constants";
@@ -17,14 +17,15 @@ export const AppContextProvider: React.FC<AppContextProviderProps> = ({
     []
   );
 
-  const ethClient = useClient({ chainId });
+  const publicClient = usePublicClient({ chainId });
+  const { data: walletClient } = useWalletClient({ chainId });
 
   const mudWeb3 = React.useMemo(() => {
-    if (!ethClient) {
+    if (!publicClient) {
       throw new Error(`Unable to retrieve Viem client for chain ${chainId}.`);
     }
-    return createMudWeb3Client({ worldAddress, ethClient });
-  }, [ethClient]);
+    return createMudWeb3Client({ worldAddress, publicClient, walletClient });
+  }, [publicClient, walletClient]);
 
   return (
     <AppContext.Provider value={{ mudSql, mudWeb3 }}>
