d6daacfdf0dbee286b3926747ab32fd5799fb810
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-01-28 18:53:26 +0100

    Move config page to dev section

--- src/pages/Dev.tsx
+++ src/pages/Dev.tsx
@@ -5,13 +5,15 @@ import Error404 from "./Error404";
 import DevMudSql from "./DevMudSql";
 import DevWeb3 from "./DevWeb3";
 import DevInternal from "./DevInternal";
+import DevConfig from "./DevConfig";
 
 const routesMap: Record<string, number> = {
   "/dev": 0,
   "/dev/": 0,
   "/dev/web3": 0,
   "/dev/mudsql": 1,
-  "/dev/internal": 2,
+  "/dev/config": 2,
+  "/dev/internal": 3,
 };
 
 const Dev: React.FC = () => {
@@ -27,6 +29,7 @@ const Dev: React.FC = () => {
         <Tabs value={currentTab} variant="scrollable" scrollButtons>
           <Tab label="Web3" component={NavLink} to="/dev/web3" />
           <Tab label="MUD Sql" component={NavLink} to="/dev/mudsql" />
+          <Tab label="Config" component={NavLink} to="/dev/config" />
           {import.meta.env.DEV && (
             <Tab label="Internal" component={NavLink} to="/dev/internal" />
           )}
@@ -36,6 +39,7 @@ const Dev: React.FC = () => {
         <Route path="" element={<DevMudSql />} />
         <Route path="/web3" element={<DevWeb3 />} />
         <Route path="/mudsql" element={<DevMudSql />} />
+        <Route path="/config" element={<DevConfig />} />
         {import.meta.env.DEV && (
           <Route path="/internal" element={<DevInternal />} />
         )}
