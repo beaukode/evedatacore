c897e7c2b6e2c7f0cd5fbf56b95f6cf4193a6e64
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-01-29 22:05:13 +0100

    Extract the distance calculator to a tool function

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -124,3 +124,22 @@ export function toJson(value: unknown): string {
     2
   );
 }
+
+export type Location = { x: string; y: string; z: string };
+
+export function lyDistance(locationA: Location, locationB: Location): number {
+  const s1x = new Big(locationA.x);
+  const s1y = new Big(locationA.y);
+  const s1z = new Big(locationA.z);
+  const s2x = new Big(locationB.x);
+  const s2y = new Big(locationB.y);
+  const s2z = new Big(locationB.z);
+  const meters = s1x
+    .minus(s2x)
+    .pow(2)
+    .plus(s1y.minus(s2y).pow(2))
+    .plus(s1z.minus(s2z).pow(2))
+    .sqrt();
+  const ly = meters.div(new Big(9.46073047258e15));
+  return ly.toNumber();
+}
