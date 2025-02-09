809c869143342a98faf6f57b29d84648e6d8f4d5
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-02-09 23:14:55 +0100

    User menu

--- src/components/web3/UserButton.tsx
+++ src/components/web3/UserButton.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import { Avatar, Button } from "@mui/material";
-import UnknwonUserIcon from "@mui/icons-material/QuestionMark";
-import { useQuery } from "@tanstack/react-query";
+import {
+  Avatar,
+  Button,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import { Hex } from "viem";
 import { useDisconnect } from "wagmi";
+import { NavLink } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+import UnknwonUserIcon from "@mui/icons-material/QuestionMark";
+import DisconnectIcon from "@mui/icons-material/PowerOff";
+import CharacterPageIcon from "@mui/icons-material/Person";
+import CopyIcon from "@mui/icons-material/ContentCopy";
 import { shorten } from "@/tools";
 import { useMudWeb3 } from "@/contexts/AppContext";
 
@@ -12,8 +23,17 @@ interface UserButtonProps {
 }
 
 const UserButton: React.FC<UserButtonProps> = ({ address }) => {
+  const menuAnchor = React.useRef<HTMLButtonElement | null>(null);
+  const [showMenu, setShowMenu] = React.useState(false);
   const { disconnect } = useDisconnect();
+
   const mudWeb3 = useMudWeb3();
+
+  const copyAddress = React.useCallback(() => {
+    navigator.clipboard.writeText(address).catch((e) => {
+      console.error("Fail to copying content", e);
+    });
+  }, [address]);
 
   const userNameQuery = useQuery({
     queryKey: ["User", address],
@@ -26,38 +46,84 @@ const UserButton: React.FC<UserButtonProps> = ({ address }) => {
       }),
   });
 
-  React.useEffect(() => {
-    console.log("UserButton rendered", address);
-  }, [address, mudWeb3]);
-
   return (
-    <Button
-      onClick={() => {
-        disconnect();
-      }}
-      sx={{
-        paddingRight: 1,
-      }}
-      variant="outlined"
-      endIcon={
-        userNameQuery.data ? (
-          <Avatar
-            sx={{ my: "-5px", mr: "-4px" }}
-            variant="rounded"
-            src="https://images.dev.quasar.reitnorf.com/Character/123456789_256.jpg"
-          />
-        ) : (
-          <Avatar
-            sx={{ my: "-5px", mr: "-4px", background: "black" }}
-            variant="rounded"
-          >
-            <UnknwonUserIcon color="primary" />
-          </Avatar>
-        )
-      }
-    >
-      <>{userNameQuery.data || shorten(address, 8)}</>
-    </Button>
+    <>
+      <Button
+        ref={menuAnchor}
+        id="user-menu-button"
+        onClick={() => {
+          setShowMenu(true);
+        }}
+        sx={{
+          paddingRight: 1,
+        }}
+        variant="outlined"
+        endIcon={
+          userNameQuery.data ? (
+            <Avatar
+              sx={{ my: "-5px", mr: "-4px" }}
+              variant="rounded"
+              src="https://images.dev.quasar.reitnorf.com/Character/123456789_256.jpg"
+            />
+          ) : (
+            <Avatar
+              sx={{ my: "-5px", mr: "-4px", background: "black" }}
+              variant="rounded"
+            >
+              <UnknwonUserIcon color="primary" />
+            </Avatar>
+          )
+        }
+      >
+        <>{userNameQuery.data || shorten(address, 8)}</>
+      </Button>
+      <Menu
+        anchorEl={menuAnchor.current}
+        open={showMenu}
+        onClose={() => {
+          setShowMenu(false);
+        }}
+        MenuListProps={{
+          "aria-labelledby": "user-menu-button",
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem
+          component={NavLink}
+          to={`/explore/characters/${address}`}
+          onClick={() => {
+            setShowMenu(false);
+          }}
+        >
+          <ListItemIcon>
+            <CharacterPageIcon color="primary" fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Character page</ListItemText>
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            copyAddress();
+            setShowMenu(false);
+          }}
+        >
+          <ListItemIcon>
+            <CopyIcon color="primary" fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>{shorten(address, 10)}</ListItemText>
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            disconnect();
+          }}
+        >
+          <ListItemIcon>
+            <DisconnectIcon color="primary" fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Disconnect</ListItemText>
+        </MenuItem>
+      </Menu>
+    </>
   );
 };
 export default React.memo(UserButton);
