f90fe99d02d756f91f770cd5efdc16644d346f39
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-02-23 21:44:42 +0100

    Fix datatable height

--- src/components/layouts/DataTableLayout.tsx
+++ src/components/layouts/DataTableLayout.tsx
@@ -28,39 +28,40 @@ const DataTableLayout = <T extends Record<string, unknown>>({
       <Helmet>
         <title>{title}</title>
       </Helmet>
-      <Box p={2} flexGrow={1} overflow="hidden">
-        <Paper
-          elevation={1}
-          sx={{
-            p: 2,
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <Box display="flex" alignItems="flex-end">
-            <Box display="flex" flexGrow="1">
-              {children}
-            </Box>
-            <Box textAlign="right" ml={2}>
-              <Typography variant="caption" color="textPrimary">
-                {data.length} {title}
-              </Typography>
-            </Box>
+
+      <Paper
+        elevation={1}
+        sx={{
+          p: 2,
+          m: 2,
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 1,
+        }}
+      >
+        <Box display="flex" alignItems="flex-end">
+          <Box display="flex" flexGrow="1">
+            {children}
           </Box>
-          <Box mt={2}>
-            <LinearProgress
-              sx={{ visibility: loading ? "visible" : "hidden" }}
-            />
+          <Box textAlign="right" ml={2}>
+            <Typography variant="caption" color="textPrimary">
+              {data.length} {title}
+            </Typography>
           </Box>
+        </Box>
+        <Box mt={2}>
+          <LinearProgress sx={{ visibility: loading ? "visible" : "hidden" }} />
+        </Box>
+        <Box flexGrow={1} flexBasis={100} overflow="hidden">
           <DataTable
             data={data}
             columns={columns}
             itemContent={itemContent}
             rememberScroll
           />
-        </Paper>
-      </Box>
+        </Box>
+      </Paper>
     </>
   );
 };
