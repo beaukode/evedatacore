7e1298823f779ce1ef84f7d341947069dc2d359e
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-03-16 21:17:33 +0100

    Setup routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,6 @@
-import express from "express";
+import { app } from "./server";
 
-export const app = express();
-app.disable("x-powered-by");
 const port = process.env.PORT || 3000;
-
-app.get("/api", (req, res) => {
-  res.send("Hello, World!");
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
