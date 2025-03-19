00ae02ca47c619ea091ac9421611063ecaae1e72
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-03-19 20:47:50 +0100

    Dont recreate services on each requests

--- backend/src/middlewares/MiddlewareServices.ts
+++ backend/src/middlewares/MiddlewareServices.ts
@@ -8,25 +8,25 @@ type DiServices<T extends DiContainerServices> = {
   [K in keyof T]: T[K] extends FunctionReturning<unknown> ? ReturnType<T[K]> : T[K];
 };
 
+const services = {
+  env: createEnvVariablesService,
+  solarSystems: createSolarSystemsService,
+  pathFinder: createPathFinderService,
+};
+
+const di = createContainer({
+  strict: true,
+});
+const resolvers = Object.entries(services).reduce((acc, [key, value]) => {
+  return {
+    ...acc,
+    [key]: typeof value === "function" ? asFunction(value as FunctionReturning<unknown>).scoped() : asValue(value),
+  };
+}, {});
+di.register(resolvers);
+
 export const middlewareServices = new Middleware({
   handler: async () => {
-    const services = {
-      env: createEnvVariablesService,
-      solarSystems: createSolarSystemsService,
-      pathFinder: createPathFinderService,
-    };
-
-    const di = createContainer({
-      strict: true,
-    });
-    const resolvers = Object.entries(services).reduce((acc, [key, value]) => {
-      return {
-        ...acc,
-        [key]: typeof value === "function" ? asFunction(value as FunctionReturning<unknown>).scoped() : asValue(value),
-      };
-    }, {});
-    di.register(resolvers);
-
     return { services: di.cradle as DiServices<typeof services> };
   },
 });
