7853dbbdae4ae16f3fabc3d6fe16b78722e0891f
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-03-19 20:39:34 +0100

    Switch backend build to esbuild

---
+++ backend/build.js
@@ -0,0 +1,13 @@
+const esbuild = require("esbuild");
+
+esbuild
+  .build({
+    entryPoints: ["src/lambda.ts"],
+    bundle: true,
+    platform: "node",
+    target: "node20",
+    outdir: "dist",
+    format: "esm",    
+    external: ["@aws-sdk"], // Exclude AWS SDK from the bundle
+  })
+  .catch(() => process.exit(1));
