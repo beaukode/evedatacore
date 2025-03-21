ae0e35f09e17ef990ddebaddfad895de41935fd2
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-03-21 22:42:32 +0100

    MudSqlService: Clean debug log & export type

--- backend/src/services/mudSql.ts
+++ backend/src/services/mudSql.ts
@@ -1,12 +1,13 @@
 import { EnvVariablesService } from "./envVariables";
 import { createMudSqlClient } from "@shared/mudsql/client";
 
+export type MudSqlService = ReturnType<typeof createMudSqlService>;
+
 interface MudSqlServiceConfig {
   env: EnvVariablesService;
 }
 
 export function createMudSqlService({ env }: MudSqlServiceConfig) {
-  console.log("Creating mudSql service");
   return createMudSqlClient({
     indexerBaseUrl: env.MUDSQL_INDEXER_BASE_URL,
     worldAddress: env.WORLD_ADDRESS,
