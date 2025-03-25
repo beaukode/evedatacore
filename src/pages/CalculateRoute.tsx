2c4fa659924820e0d08bb5a371a76e134968bc5c
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-03-25 21:27:51 +0100

    Remove credit message

--- src/pages/CalculateRoute.tsx
+++ src/pages/CalculateRoute.tsx
@@ -3,7 +3,6 @@ import { Alert, Box, Grid2 as Grid } from "@mui/material";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { Helmet } from "react-helmet";
 import PaperLevel1 from "@/components/ui/PaperLevel1";
-import ExternalLink from "@/components/ui/ExternalLink";
 import { getCalcPathFromTo } from "@/api/evedatacore";
 import RoutePlannerForm from "./Calculators/RoutePlannerForm";
 import RoutePlannerRoute from "./Calculators/RoutePlannerRoute";
@@ -105,19 +104,6 @@ const CalculateRoute: React.FC = () => {
           )}
         </Grid>
       </Grid>
-      <Alert severity="info">
-        This calculator is based on the work of Shish, from his website{" "}
-        <ExternalLink
-          title="EVE Frontier Toolbox"
-          href="https://eftb.shish.io/"
-        />{" "}
-        and its source code
-        <br />
-        <br />
-        If you enjoy this calculator, please consider supporting him by buying a
-        coffee at{" "}
-        <ExternalLink title="Buy a coffee" href="https://ko-fi.com/shish2k" />
-      </Alert>
     </Box>
   );
 };
