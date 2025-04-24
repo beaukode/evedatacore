14ac2bf9914f50ecf16d97feeb6d369af5c490f3
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-04-24 22:19:12 +0200

    Add option to display as inline block

--- src/components/ui/SolarsystemName.tsx
+++ src/components/ui/SolarsystemName.tsx
@@ -4,15 +4,20 @@ import { useSolarSystemsIndex } from "@/contexts/AppContext";
 
 interface ButtonSolarsystemProps {
   solarSystemId?: string | number;
+  inline?: boolean;
 }
 
 const SolarsystemName: React.FC<ButtonSolarsystemProps> = ({
   solarSystemId,
+  inline = false,
 }) => {
   const solarSystems = useSolarSystemsIndex();
 
   if (solarSystemId === undefined) return null;
-  if (!solarSystems) return <Skeleton width={80} />;
+  if (!solarSystems)
+    return (
+      <Skeleton sx={{ display: inline ? "inline-block" : "block" }} width={80} />
+    );
 
   const solarSystem = solarSystems.getById(solarSystemId.toString());
   if (!solarSystem) return null;
