0a90ad7250a4ddcd0a7c83b17d5fb6124249a800
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-04-29 08:39:30 +0200

    Set v2 world address

--- src/config/web3Config.prod.ts
+++ src/config/web3Config.prod.ts
@@ -1,3 +1,3 @@
 export const indexerBaseUrl = "https://indexer.mud.pyropechain.com";
-export const worldAddress = "0x90373cf89e73168cdf90e99d0a7fa9c4b5625c6a";
+export const worldAddress = "0xd5bd059a108888523d428e60ae515f0769a9b562";
 export const chainId = 695569;
