c37428e05009670bf0bf5b8dceff54d6b884a535
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-06-17 20:55:39 +0200

    Clean unused import

--- shared/mudsql/queries/listAssemblies.ts
+++ shared/mudsql/queries/listAssemblies.ts
@@ -4,7 +4,6 @@ import { MudSqlClient } from "../client";
 import { ensureArray, toSqlHex } from "../utils";
 import {
   Assembly,
-  AssemblyState,
   AssemblyType,
   assemblyTypeMap,
   assemblyTypeReverseMap,
