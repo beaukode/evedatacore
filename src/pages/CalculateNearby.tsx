b1d0ce7f398a02721632853a7a81f4878b8f20f8
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-06-18 20:17:51 +0200

    Nearby search results

--- src/pages/CalculateNearby.tsx
+++ src/pages/CalculateNearby.tsx
@@ -1,5 +1,17 @@
 import React from "react";
-import { Alert, Box, Grid2 as Grid } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Grid2 as Grid,
+  Table,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableBody,
+  Typography,
+  Tooltip,
+  Button,
+} from "@mui/material";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { Helmet } from "react-helmet";
 import PaperLevel1 from "@/components/ui/PaperLevel1";
@@ -7,11 +19,13 @@ import { getFindNearIdDistance } from "@/api/evedatacore";
 import { useSolarSystemsIndex } from "@/contexts/AppContext";
 import useCharacter from "@/tools/useCharacter";
 import NearbyForm from "./Calculators/NearbyForm";
+import ButtonSolarsystem from "@/components/buttons/ButtonSolarsystem";
 
 type SubmitHandler = React.ComponentProps<typeof NearbyForm>["onSubmit"];
 type NearbyFormData = Parameters<SubmitHandler>[0];
 
 const CalculateNearby: React.FC = () => {
+  const [copyError, setCopyError] = React.useState(false);
   const [queryData, setQueryData] = React.useState<NearbyFormData>();
   const solarSystemsIndex = useSolarSystemsIndex();
   const queryClient = useQueryClient();
@@ -53,6 +67,28 @@ const CalculateNearby: React.FC = () => {
     [queryClient]
   );
 
+  const copyResults = React.useCallback(() => {
+    setCopyError(false);
+    if (!solarSystemsIndex || !query.data || query.data.length < 1) {
+      return;
+    }
+    let content = `From ${fromName} up to ${queryData?.distance} Ly\n`;
+    content += query.data
+      ?.map(
+        (item) =>
+          `<a href="showinfo:5//${item.solarSystemId}">${solarSystemsIndex.getById(item.solarSystemId.toString())?.solarSystemName}</a> ${item.distance}Ly`
+      )
+      .join("\n");
+    navigator.clipboard
+      .writeText(content)
+      .then(() => {
+        setCopyError(false);
+      })
+      .catch(() => {
+        setCopyError(true);
+      });
+  }, [query.data, queryData, fromName, solarSystemsIndex]);
+
   return (
     <Box p={2} flexGrow={1} overflow="auto">
       <Helmet>
@@ -84,7 +120,55 @@ const CalculateNearby: React.FC = () => {
                 <Alert severity="error">{query.error.message}</Alert>
               )}
               {query.isSuccess && query.data && (
-                <Box>{JSON.stringify(query.data)}</Box>
+                <>
+                  {copyError && (
+                    <Alert severity="error" sx={{ mb: 2 }}>
+                      Failed to copy to clipboard
+                    </Alert>
+                  )}
+                  <Box display="flex" alignItems="flex-start" mb={1}>
+                    <Box flexGrow={1}>
+                      <Typography variant="body1">
+                        {query.data.length === 1
+                          ? "1 system found"
+                          : `${query.data.length} systems found`}
+                      </Typography>
+                    </Box>
+                    <Box>
+                      {query.data.length > 0 && (
+                        <Tooltip title="If you paste with EVE-Links into in-game notepad, you get clickable links">
+                          <Button
+                            variant="outlined"
+                            size="small"
+                            onClick={copyResults}
+                          >
+                            Copy
+                          </Button>
+                        </Tooltip>
+                      )}
+                    </Box>
+                  </Box>
+                  <Table size="small" stickyHeader>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>System</TableCell>
+                        <TableCell>Distance</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {query.data.map((item) => (
+                        <TableRow key={item.solarSystemId}>
+                          <TableCell>
+                            <ButtonSolarsystem
+                              solarSystemId={item.solarSystemId}
+                            />
+                          </TableCell>
+                          <TableCell>{item.distance}Ly</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </>
               )}
             </PaperLevel1>
           )}
