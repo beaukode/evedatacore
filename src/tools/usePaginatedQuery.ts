daad08a1aa7c98b43ef0a6d0716772d3aeca10e4
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-07-10 19:46:05 +0200

    Limit usePaginatedQuery auto pagination to 20 pages

--- src/tools/usePaginatedQuery.ts
+++ src/tools/usePaginatedQuery.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
 interface UsePaginatedQueryOptions<
@@ -12,11 +12,14 @@ interface UsePaginatedQueryOptions<
 
 type ExtractItemType<T> = T extends { items: (infer I)[] } ? I : never;
 
+const MAX_PAGES = 20;
+
 export function usePaginatedQuery<
   T extends { nextKey?: string; items: unknown[] },
 >(
   options: UsePaginatedQueryOptions<T>
 ): { data?: ExtractItemType<T>[]; isFetching: boolean } {
+  const [page, setPage] = useState(1);
   const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
     queryKey: options.queryKey,
     queryFn: options.queryFn,
@@ -27,10 +30,11 @@ export function usePaginatedQuery<
   });
 
   useEffect(() => {
-    if (hasNextPage && !isFetching) {
+    if (hasNextPage && !isFetching && page < MAX_PAGES) {
+      setPage((s) => s + 1);
       fetchNextPage();
     }
-  }, [hasNextPage, isFetching, fetchNextPage]);
+  }, [hasNextPage, isFetching, fetchNextPage, page]);
 
   const memoizedData = useMemo(() => {
     return data?.pages.flatMap((p) => p?.items ?? []) as ExtractItemType<T>[];
