805433582ed10da6c9d7880e111de50648742319
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-10-05 09:50:17 +0200

    API client update
    API client update

--- src/api/evedatacore-v2/index.ts
+++ src/api/evedatacore-v2/index.ts
@@ -1,6 +1,8 @@
 export * from "./generated";
-import { client } from "./generated";
+import { client, GetCharacterIdLogbookResponse } from "./generated";
 
 client.setConfig({
   baseUrl: "/api-v2",
 });
+
+export type LogBookRecord = GetCharacterIdLogbookResponse["items"][number];
