c3d3577ad35c01ffd45e0d7e8a39a784f334a0c9
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-10-13 20:52:04 +0200

    Make kills table sortable

--- src/api/evedatacore-v2/index.ts
+++ src/api/evedatacore-v2/index.ts
@@ -5,6 +5,7 @@ import {
   GetCharactersResponse,
   GetTribesResponse,
   GetAssembliesTypeStateResponse,
+  GetKillsResponse
 } from "./generated";
 
 client.setConfig({
@@ -15,3 +16,4 @@ export type LogBookRecord = GetCharacterIdLogbookResponse["items"][number];
 export type Tribe = GetTribesResponse["items"][number];
 export type Character = GetCharactersResponse["items"][number];
 export type Assembly = GetAssembliesTypeStateResponse["items"][number];
+export type Kill = GetKillsResponse["items"][number];
