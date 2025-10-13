d7d3b89b2129a73f95c7098db607d0da866a7503
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-10-13 20:28:13 +0200

    Make tribes table sortable

--- src/api/evedatacore-v2/index.ts
+++ src/api/evedatacore-v2/index.ts
@@ -1,8 +1,10 @@
 export * from "./generated";
-import { client, GetCharacterIdLogbookResponse } from "./generated";
+import { client, GetCharacterIdLogbookResponse, GetCharactersResponse, GetTribesResponse } from "./generated";
 
 client.setConfig({
   baseUrl: "/api-v2",
 });
 
 export type LogBookRecord = GetCharacterIdLogbookResponse["items"][number];
+export type Tribe = GetTribesResponse["items"][number];
+export type Character = GetCharactersResponse["items"][number];
