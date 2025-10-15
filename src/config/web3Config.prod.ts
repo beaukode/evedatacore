b1e33b5b4ac14ac1d0705aa27a136a7c1bfa1951
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-10-15 15:06:36 +0200

    Update world address

--- src/config/web3Config.prod.ts
+++ src/config/web3Config.prod.ts
@@ -1,3 +1,3 @@
 export const indexerBaseUrl = "https://indexer.mud.pyropechain.com";
-export const worldAddress = "0x7085f3e652987f656fb8dee5aa6592197bb75de8";
+export const worldAddress = "0x7b71fe480ac4e7e96d150a1454411c5cbfb2b1f1";
 export const chainId = 695569;
