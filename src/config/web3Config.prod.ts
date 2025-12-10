c17c72d955428b6728b5cc2e139e6972dbd971e5
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-12-10 12:17:26 +0100

    Switch to OP Sepolia chain

--- src/config/web3Config.prod.ts
+++ src/config/web3Config.prod.ts
@@ -1,4 +1,4 @@
 export const worldAddress = "0x7b71fe480ac4e7e96d150a1454411c5cbfb2b1f1";
-export const chainId = 695569;
+export const chainId = 11155420;
 export const explorerBaseUrl = "https://testnet-explorer.optimism.io";
 export const web3NetworkName = "OP Sepolia";
