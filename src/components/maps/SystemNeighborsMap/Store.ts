59a1213d952c3bbe91bf90026d5093136fa2417f
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-12-17 22:04:18 +0100

    Make store initialize only when map component is mounted and implement a saga dispose action

--- src/components/maps/SystemNeighborsMap/Store.ts
+++ src/components/maps/SystemNeighborsMap/Store.ts
@@ -4,22 +4,36 @@ import createSagaMiddleware from "redux-saga";
 import { SNMRootSaga } from "./Sagas";
 import slice from "./Slice";
 
-const sagaMiddleware = createSagaMiddleware();
-const middleware = [sagaMiddleware];
+export type SNMStore = ReturnType<typeof createSNMStore>;
+let store: SNMStore | undefined = undefined;
 
-export const SNMStore = configureStore({
-  reducer: { map: slice.reducer },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(middleware),
-});
+function createSNMStore() {
+  const sagaMiddleware = createSagaMiddleware();
+  const middleware = [sagaMiddleware];
+  const store = configureStore({
+    reducer: { map: slice.reducer },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(middleware),
+  });
+  sagaMiddleware.run(SNMRootSaga);
+  return store;
+}
 
-sagaMiddleware.run(SNMRootSaga);
+export function getSNMStore() {
+  if (store) {
+    return store;
+  }
+
+  store = createSNMStore();
+
+  return store;
+}
 
 export const SNMSelectors = slice.selectors;
 export const SNMActions = slice.actions;
 
-export type SNMState = ReturnType<typeof SNMStore.getState>;
-export type SNMDispatch = typeof SNMStore.dispatch;
+export type SNMState = ReturnType<SNMStore["getState"]>;
+export type SNMDispatch = SNMStore["dispatch"];
 
 export const useSNMDispatch = useDispatch.withTypes<SNMDispatch>();
 export const useSNMSelector = useSelector.withTypes<SNMState>();
