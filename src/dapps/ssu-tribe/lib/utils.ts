36e77ebf7e4a9470c851daf6defc1919537e58d5
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-12-26 12:05:41 +0100

    Update the SSU tribe dapp url to leave space for more ssu dapps

--- src/dapps/ssu-tribe/lib/utils.ts
+++ src/dapps/ssu-tribe/lib/utils.ts
@@ -7,19 +7,20 @@ export type SSUAssembly = Assembly & {
 };
 
 export function getDappUrl(): string {
-  const VITE_DAPP_SSU_URL = import.meta.env.VITE_DAPP_SSU_URL;
-  if (!VITE_DAPP_SSU_URL) {
-    throw new Error("VITE_DAPP_SSU_URL is not set");
+  const VITE_DAPP_SSU_TRIBE_URL = import.meta.env.VITE_DAPP_SSU_TRIBE_URL;
+  if (!VITE_DAPP_SSU_TRIBE_URL) {
+    throw new Error("VITE_DAPP_SSU_TRIBE_URL is not set");
   }
-  return VITE_DAPP_SSU_URL;
+  return VITE_DAPP_SSU_TRIBE_URL;
 }
 
 export function getSsuSystemId(): Hex {
-  const VITE_DAPP_SSU_SYSTEM_ID = import.meta.env.VITE_DAPP_SSU_SYSTEM_ID;
-  if (!isHex(VITE_DAPP_SSU_SYSTEM_ID)) {
-    throw new Error("VITE_DAPP_SSU_SYSTEM_ID format is invalid");
+  const VITE_DAPP_SSU_TRIBE_SYSTEM_ID = import.meta.env
+    .VITE_DAPP_SSU_TRIBE_SYSTEM_ID;
+  if (!isHex(VITE_DAPP_SSU_TRIBE_SYSTEM_ID)) {
+    throw new Error("VITE_DAPP_SSU_TRIBE_SYSTEM_ID format is invalid");
   }
-  return VITE_DAPP_SSU_SYSTEM_ID;
+  return VITE_DAPP_SSU_TRIBE_SYSTEM_ID;
 }
 
 export function computeDappUrl(ssuId: bigint | string): string {
