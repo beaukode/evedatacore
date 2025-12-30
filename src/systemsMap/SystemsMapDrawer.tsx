bfde23b725d9b8665d4d8b7e8bf4a3583c2690de
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-12-30 23:15:46 +0100

    Rework map drawer, remove accordion component usage and make selected system panel grow

--- src/systemsMap/SystemsMapDrawer.tsx
+++ src/systemsMap/SystemsMapDrawer.tsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { Paper } from "@mui/material";
 import PanelDisplay from "./components/PanelDisplay";
 import PanelSelectedSystem from "./components/PanelSelectedSystem";
 import PanelTool from "./components/PanelTool";
 
 const SystemsMapDrawer: React.FC = () => {
   return (
-    <>
+    <Paper
+      sx={{
+        width: 300,
+        flexShrink: 0,
+        flexGrow: 0,
+        overflowY: "auto",
+        overflowX: "hidden",
+        display: "flex",
+        flexDirection: "column",
+        gap: 0,
+      }}
+      elevation={4}
+    >
       <PanelDisplay />
       <PanelTool />
       <PanelSelectedSystem />
-    </>
+    </Paper>
   );
 };
 
---
+++ src/systemsMap/components/Panel.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Box, Paper, SxProps, Typography, useTheme } from "@mui/material";
+
+interface PanelHeaderProps {
+  title: string;
+  titleAdornment?: React.ReactNode;
+  sx?: SxProps;
+  children?: React.ReactNode;
+}
+
+const Panel: React.FC<PanelHeaderProps> = ({
+  title,
+  titleAdornment,
+  sx,
+  children,
+}) => {
+  const theme = useTheme();
+  return (
+    <Paper
+      elevation={4}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 0,
+        mb: theme.spacing(1),
+        ...sx,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          px: 2,
+          py: 1,
+          borderBottom: `2px dashed ${theme.palette.divider}`,
+        }}
+      >
+        <Typography component="h3">{title}</Typography>
+        {titleAdornment}
+      </Box>
+      <Box
+        p={1}
+        display="flex"
+        flexDirection="column"
+        flexGrow={1}
+        flexShrink={1}
+        alignItems="center"
+      >
+        {children}
+      </Box>
+    </Paper>
+  );
+};
+
+export default Panel;
