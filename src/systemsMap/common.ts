60045da47d4d17d077e9d54acb60f10db1d43f93
Jérémie COLOMBO <5773637+beaukode@users.noreply.github.com>
2025-12-30 09:52:54 +0100

    Update POI

--- src/systemsMap/common.ts
+++ src/systemsMap/common.ts
@@ -42,7 +42,7 @@ export type GraphConnnection = {
   target: string;
 };
 
-export type PointOfInterestIcon = "shield" | "circle";
+export type PointOfInterestIcon = "shield" | "circle" | "moon";
 
 export type PointOfInterest = {
   name: string;
@@ -55,12 +55,20 @@ export const pointOfInterests: Array<PointOfInterest> = [
   { name: "Platinum Mass Cluster", color: "silver" },
   { name: "Hydrosulphide Formation", color: "dodgerblue" },
   { name: "Natural Asteroid Cluster", color: "green" },
-  { name: "Metal Rich", color: "gray" },
-  { name: "Rocky Asteroid", color: "brown" },
+  { name: "Metal-Rich Cluster", color: "gray" },
+  { name: "Murky Outer Field", color: "orange" },
+  { name: "Rocky Asteroid Field", color: "brown" },
+  { name: "Carbon Rock Debris Field ", color: "black" },
   { name: "Rift", color: "gold" },
   { name: "Minor Drone Nest", color: "dodgerblue", icon: "shield" },
   { name: "Feral Enclave", color: "orange", icon: "shield" },
   { name: "Domination Cluster", color: "red", icon: "shield" },
+  { name: "Ruined Bombardment Cannon", color: "gray", icon: "shield" },
+  { name: "Inculcator Wreckage ", color: "orange", icon: "shield" },
+  { name: "Razed Inculcator", color: "green", icon: "shield" },
+  { name: "Ruined Inculcator", color: "brown", icon: "shield" },
   { name: "Unmoored Silo", color: "pink", icon: "circle" },
-  { name: "Ruined Inculator", color: "coral", icon: "circle" },
+  { name: "Abandoned Nexus Core", color: "dodgerblue", icon: "moon" },
+  { name: "Abandoned Processing Plant", color: "brown", icon: "moon" },
+  { name: "Destroyed Research Archive", color: "gold", icon: "moon" },
 ];
